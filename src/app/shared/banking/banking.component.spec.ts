debed5706b3c65b24e9fe18f65a280beafcac036
Jean Gabriel <jeangabrieljb9@gmail.com>
2022-09-02 21:57:26 -0300

    teste unitários nas funções - ifs

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -20,4 +20,38 @@ describe('BankingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('(U) getPoupanca(): shoud have poupanca = 10', () => {
+    expect(component.getPoupanca).toEqual(10);
+  });
+
+  it('(U) getCarteira(): shoud have carteira = 50', () => {
+    expect(component.getCarteira).toEqual(50);
+  });
+
+  it('(U) setSacar(): shoud transfer poupanca from carteira', () => {
+    component.setSacar('10');
+    expect(component.getPoupanca).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  });
+
+  it('(U) setsacar(): shoud transfer poupanca dont have string  (isNaN) or poupanca < value', () => {
+    expect(component.setSacar('string')).not.toBeTruthy();
+    expect(component.setSacar('100')).not.toBeTruthy();
+    expect(component.getPoupanca).toEqual(10);
+    expect(component.getCarteira).toEqual(50);
+  });
+
+  it('(U) setDepositar(): shoud transfer carteira from poupanca', () => {
+    component.setDepositar('50');
+    expect(component.getCarteira).toEqual(0);
+    expect(component.getPoupanca).toEqual(60);
+  });
+
+  it('(U) setDepositar(): shoud transfer carteira dont have string  (isNaN) or carteira < value', () => {
+    expect(component.setDepositar('string')).not.toBeTruthy();
+    expect(component.setDepositar('100')).not.toBeTruthy();
+    expect(component.getPoupanca).toEqual(10);
+    expect(component.getCarteira).toEqual(50);
+  });
 });
