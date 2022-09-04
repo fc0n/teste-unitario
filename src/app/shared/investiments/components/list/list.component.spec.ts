b8f01ea0eb3ad3e905470e24e11662cdb6e600b2
Jean Gabriel <jeangabrieljb9@gmail.com>
2022-09-04 01:26:04 -0300

    teste unitário - interface com um componente dentro do outro

---
+++ src/app/shared/investiments/components/list/list.component.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListComponent } from './list.component';
+
+describe('ListComponent', () => {
+  let component: ListComponent;
+  let fixture: ComponentFixture<ListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('(U) should list investiments' , () => {
+    let investiments = component.investiments;
+    expect(investiments.length).toBe(4);
+    expect(investiments[0].name).toContain('Itaú');
+    expect(investiments[3].name).toContain('Inter');
+  });
+
+  it('(I) should list investiments' , () => {
+    let investiments = fixture.debugElement.nativeElement.querySelectorAll('.listItens');
+    expect(investiments.length).toBe(4);
+    expect(investiments[0].textContent.trim()).toEqual('Itaú | 100');
+  });
+});
