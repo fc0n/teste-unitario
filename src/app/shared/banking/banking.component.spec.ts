7e963d3a5286d4f87b17107cfbc97cb6410687d0
Jean Gabriel <jeangabrieljb9@gmail.com>
2022-09-04 15:28:05 -0300

    corrigindo alguns erros do service com import

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -3,13 +3,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BankingComponent } from './banking.component';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 describe('BankingComponent', () => {
   let component: BankingComponent;
   let fixture: ComponentFixture<BankingComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BankingComponent  , ListComponent]
+      declarations: [ BankingComponent  , ListComponent],
+      imports: [HttpClientTestingModule],
     })
     .compileComponents();
 
--- src/app/shared/investiments/components/list/list.component.spec.ts
+++ src/app/shared/investiments/components/list/list.component.spec.ts
@@ -2,13 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListComponent ]
+      declarations: [ ListComponent ],
+      imports: [HttpClientTestingModule],
     })
     .compileComponents();
 
