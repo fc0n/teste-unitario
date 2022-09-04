b8eb48ea11d98e4899568197391173e2966312ce
Jean Gabriel <jeangabrieljb9@gmail.com>
2022-09-04 13:05:14 -0300

    erro de inglês + um teste unitário que faltou

--- src/app/shared/investiments/components/list/list.component.spec.ts
+++ src/app/shared/investiments/components/list/list.component.spec.ts
@@ -32,5 +32,6 @@ describe('ListComponent', () => {
     let investiments = fixture.debugElement.nativeElement.querySelectorAll('.listItens');
     expect(investiments.length).toBe(4);
     expect(investiments[0].textContent.trim()).toEqual('Itaú | 100');
+    expect(investiments[3].textContent.trim()).toEqual('Inter | 100');
   });
 });
